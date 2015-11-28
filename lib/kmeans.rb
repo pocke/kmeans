d34b6de55e1585bbbb777f262bc41f20e946752e
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-11-29 08:14:35 +0900

    Remove require

--- lib/kmeans.rb
+++ lib/kmeans.rb
@@ -6,6 +6,6 @@ module Kmeans
   require File.dirname(__FILE__) +  "/kmeans/pearson"
   require File.dirname(__FILE__) +  "/kmeans/cluster"
   require File.dirname(__FILE__) +  "/kmeans/hcluster"
-  require File.dirname(__FILE__) +  "/kmeans/dendrogram"
+  # require File.dirname(__FILE__) +  "/kmeans/dendrogram"
   require File.dirname(__FILE__) +  "/kmeans/version"
 end
